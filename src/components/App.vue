6aefcc4eadcbdc540a1dff98ee8e7ed23ef0f96f
username <977865769@qq.com>
2016-08-14 22:17:57 +0800

    update

---
+++ src/components/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <h1>{{ msg }}</h1>
+    <button class="bs-customizer-alert">警告</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      // note: changing this line won't causes changes
+      // with hot-reload because the reloaded component
+      // preserves its current state and we are modifying
+      // its initial state.
+      msg: 'Hello Vue!'
+    }
+  }
+}
+</script>
+
+<style>
+body {
+  font-family: Helvetica, sans-serif;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,7 @@
+import Vue from 'vue'
+import App from './components/App.vue'
+require('./assets/docs.css');
+new Vue({
+  el: 'body',
+  components: { App }
+})
